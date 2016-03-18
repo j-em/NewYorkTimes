8e2e0fbad8c023b4eb96b03d4311ad5bfb4ea910
Jeremy Allard <jeremy@allard.io>
2016-03-18 14:14:00 -0400

    add main implementation of getArticles

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,30 @@
+package jem.nytimes
+import scalaj.http._
+import argonaut._, Argonaut._
+import Article._
+import java.time._
+import format._
+import scalaz._
+import Scalaz._
+import effect._
+import std._
+import syntax.std._
+
+object NyTimes {
+  type Key = String
+  type Query = String
+
+  type ArticleIO = IO[Option[List[Article]]]
+  type ArticleFetcher = Function5[String, String, LocalDate, LocalDate, Int, Option[List[Article]]]
+
+  def defaultRequestArticles(key: String, query: String, startDate: LocalDate, endDate: LocalDate, page: Int = 0): Option[List[Article]] =
+    Http("https://api.nytimes.com/svc/search/v2/articlesearch.json").params(
+      ("q", query),
+      ("begin_date", startDate.format(DateTimeFormatter.BASIC_ISO_DATE)),
+      ("end_date", endDate.format(DateTimeFormatter.BASIC_ISO_DATE)),
+      ("api-key", key),
+      ("page", page.toString)).asString.body.decodeOption[Response].flatMap(_.response).flatMap(_.docs)
+
+  def getArticles(query: String, startDate: LocalDate, endDate: LocalDate, page: Int = 0, f: ArticleFetcher = defaultRequestArticles: ArticleFetcher): Reader[String, ArticleIO] =
+    Reader((key: String) => IO(f(key, query,  startDate, endDate, page)))
+}

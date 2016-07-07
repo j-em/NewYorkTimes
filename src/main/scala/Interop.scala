ad2c544ca445bc7de6f875a94ccfc14e19448a98
j-em <jeremy@allard.io>
2016-07-06 20:34:01 -0400

    Make interop more android-friendly.
    This remove the requirement to use LocalDate which isn't available on
    Android. A Date case class is provided as a bridge to the core
    getArticles function.

--- src/main/scala/Interop.scala
+++ src/main/scala/Interop.scala
@@ -1,9 +1,14 @@
 package jem.nytimes;
+import java.time.LocalDate
 import NyTimes._
 import scalaz._
 import Scalaz._
 
 object Interop {
-  val getArticles = defaultRequestArticles _ map (_ getOrElse(null))
-}
+  case class Date(year: Int, month: Int, day: Int) {
+    def toLocalDate(): LocalDate = LocalDate.of(this.year, this.month, this.day)
+  }
 
+  def getArticles(key: String, query: String, startDate: Date, endDate: Date, page: Int = 0) =
+    defaultRequestArticles(key, query, startDate.toLocalDate(), endDate.toLocalDate(), page).getOrElse(null)
+}

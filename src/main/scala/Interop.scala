0333b1a4ba30a988e2cdee5cfe1fed4a0414c0e1
j-em <jeremy@allard.io>
2016-07-07 15:05:08 -0400

    Make the Interop functions return a Java list.
    This make sure the Interop functions are returning Java collections
    instead of Scala collections. It should be more useful this way when
    using the library outside of Scala.

--- src/main/scala/Interop.scala
+++ src/main/scala/Interop.scala
@@ -1,9 +1,13 @@
 package jem.nytimes;
 import NyTimes._
+import collection.mutable._
+import scala.collection.JavaConversions._
 import scalaz._
 import Scalaz._
+import org.joda.time.LocalDate
+import Article._
 
 object Interop {
-  val getArticles = defaultRequestArticles _ map (_ getOrElse(null))
+  def getArticles(key: String, query: String, startDate: LocalDate, endDate: LocalDate, page: scala.Int): java.util.List[Article] =
+    defaultRequestArticles(key, query, startDate, endDate, page).getOrElse(null)
 }
-

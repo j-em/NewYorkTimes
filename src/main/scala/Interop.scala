6c491e5ee12d96bf4dfd2b2f9e6ab256c6486331
j-em <jeremy@allard.io>
2016-07-07 12:53:26 -0400

    Revert "Make interop more android-friendly."
    This is a bad idea.

--- src/main/scala/Interop.scala
+++ src/main/scala/Interop.scala
@@ -1,14 +1,9 @@
 package jem.nytimes;
-import java.time.LocalDate
 import NyTimes._
 import scalaz._
 import Scalaz._
 
 object Interop {
-  case class Date(year: Int, month: Int, day: Int) {
-    def toLocalDate(): LocalDate = LocalDate.of(this.year, this.month, this.day)
-  }
-
-  def getArticles(key: String, query: String, startDate: Date, endDate: Date, page: Int = 0) =
-    defaultRequestArticles(key, query, startDate.toLocalDate(), endDate.toLocalDate(), page).getOrElse(null)
+  val getArticles = defaultRequestArticles _ map (_ getOrElse(null))
 }
+

8af44d0cbd7e35383f41181ff91af0000cc29c31
j-em <jeremy@allard.io>
2016-07-15 21:32:59 -0400

    Add initial API documentation.

---
+++ index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"jem" : [], "jem.nytimes" : [{"object" : "jem\/nytimes\/Article$.html", "name" : "jem.nytimes.Article"}, {"object" : "jem\/nytimes\/Codecs$.html", "name" : "jem.nytimes.Codecs"}, {"object" : "jem\/nytimes\/Interop$.html", "name" : "jem.nytimes.Interop"}, {"object" : "jem\/nytimes\/NyTimes$.html", "name" : "jem.nytimes.NyTimes"}]};
